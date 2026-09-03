00e0efbb8251a9d5955a8f31179a63ea4281bf60
Podmoskovnov Lew <lev.tmb.l29@gmail.com>
2026-09-03 19:02:17 +0300

    check

--- 112-blink/main.c
+++ 112-blink/main.c
@@ -6,7 +6,7 @@ const uint LED_PIN = 25;
 int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-
+    //check
     while (1) {
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
