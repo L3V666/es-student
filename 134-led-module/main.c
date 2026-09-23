bfb7511c4f44423cb3419ed62386b7df9912faec
Подмосковнов Лев <lev.tmb.l29@gmail.com>
2026-09-23 21:19:32 +0300

    Сделана п1.3.6

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -2,8 +2,8 @@
 #include "hardware/gpio.h"
 #include "hardware/regs/addressmap.h"
 #include "hardware/regs/sio.h"
-#include <stdio.h>
 #include "led.h"
+#include "log.h"
 
 const uint BUTTON_PIN = 15;
 
@@ -21,16 +21,20 @@ void handle_command(int command)
     if (command == 'e')
     {
         led_set(true);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
     }
     else if (command == 'd')
     {
         led_set(false);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+    }
+    else if (command == 'v')
+    {
+        log_version();
     }
     else
     {
-        printf("unknown command: %c\n", command);
+        LOG_ERR("unknown command: %c\n", command);
     }
 }
 
@@ -60,7 +64,7 @@ int main() {
         if (previous == true && current == false)
         {
             led_toggle();
-            printf("led %s\n", led_is_on() ? "on" : "off");
+            LOG_INF("led %s\n", led_is_on() ? "on" : "off");
         }
 
         // запоминаем текущее состояние пина кнопки, как предыдущее
@@ -74,6 +78,7 @@ int main() {
             continue;
         }
 
+        LOG_DBG("got %c\n", command);
         handle_command(command);
     }
 }
